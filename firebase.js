b380d15e6271a036774052861d59ad4857b52c2c
Adel Kouaou <adel.kouaou@hotmail.com>
2023-11-24 22:40:09 -0500

    fix: env variable name

--- firebase.js
+++ firebase.js
@@ -10,10 +10,10 @@ import firebase from "firebase";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId:process.env.PROJECT_ID,
-  storageBucket:process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGIN_SENDER_ID,
-  appId: process.env.APP_ID
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase if not initialized yet
